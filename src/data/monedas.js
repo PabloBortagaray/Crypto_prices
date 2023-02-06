45db5c21c7f7804a367f0146b121bd3bcadd3d2d
Your Name <pa.bortagaray@gmail.com>
2023-02-06 03:43:45 -0300

    Crypto

---
+++ src/data/monedas.js
@@ -0,0 +1,10 @@
+const monedas = [
+    {id: 'USD', nombre: 'Dolar de Estados Unidos'},
+    {id: 'MXN', nombre: 'Pesos Mexicanos'},
+    {id: 'EUR', nombre: 'Euros'},
+    {id: 'GBP', nombre: 'Libras Esterlinas'},
+]
+
+export {
+    monedas
+}
